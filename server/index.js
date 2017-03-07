2d9d5675c8170afdb8cdb328e223b4837bd8e6af
Rachomir <bciach@thebrain.pro>
2017-03-07 11:53:19 +0000

    Lint code

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
+import {createApolloServer} from 'meteor/apollo';
+import {makeExecutableSchema} from 'graphql-tools';
 import schema from './schema';
 import resolvers from './resolvers';
 
 const executableSchema = makeExecutableSchema({
-  typeDefs: schema,
-  resolvers,
+    typeDefs: schema,
+    resolvers,
 });
 
 createApolloServer({
-  schema: executableSchema
+    schema: executableSchema
 });
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,58 +1,58 @@
 const channels = [];
 let ID = 0;
 
-const getNextID = ()=> {
-  ID += 1;
-  return ID;
-}
+const getNextID = () => {
+    ID += 1;
+    return ID;
+};
 
 const resolvers = {
-  Query: {
-    channels() {
-      return channels;
+    Query: {
+        channels() {
+            return channels;
+        },
+        channel(obj, args, context) {
+            console.log("JMOZGAWA: channels", channels);
+            return channels.find(channel => channel.name === args.name);
+        }
     },
-    channel(obj, args, context) {
-      console.log("JMOZGAWA: channels", channels);
-      return channels.find(channel=>channel.name === args.name);
-    }
-  },
-  Mutation: {
-    post(obj, args, context) {
-      const targetChannel = channels.find(channelItem=>channelItem.name === args.channel);
-      if (targetChannel) {
-        const newMessage = {
-          id : getNextID(),
-          handle: "USER NAME",
-          content: args.message,
-          timestamp: Math.round(+new Date()/1000)
-        };
-        targetChannel.messages.push(newMessage);
+    Mutation: {
+        post(obj, args, context) {
+            const targetChannel = channels.find(channelItem => channelItem.name === args.channel);
+            if (targetChannel) {
+                const newMessage = {
+                    id: getNextID(),
+                    handle: "USER NAME",
+                    content: args.message,
+                    timestamp: Math.round(+new Date() / 1000)
+                };
+                targetChannel.messages.push(newMessage);
 
-        return newMessage;
-      } else {
-        throw new Error('channel not found');
-      }
+                return newMessage;
+            } else {
+                throw new Error('channel not found');
+            }
+        },
+
+        createChannel(obj, args, context) {
+            const newChannel = {
+                id: getNextID(),
+                name: args.name,
+                messages: []
+            };
+
+            console.log("JMOZGAWA: newChannel", newChannel);
+            channels.push(newChannel);
+            return newChannel;
+        },
+
+        removeChannel(obj, args, context) {
+            const targetChannelIndex = channels.indexOf(channelItem => channelItem.name === args.channel);
+            return channels.splice(targetChannelIndex, 1).length > 0;
+
+        }
+
     },
-
-    createChannel(obj, args, context) {
-      const newChannel = {
-        id: getNextID(),
-        name: args.name,
-        messages: []
-      };
-
-      console.log("JMOZGAWA: newChannel",newChannel);
-      channels.push(newChannel);
-      return newChannel;
-    },
-
-    removeChannel(obj, args, context) {
-      const targetChannelIndex = channels.indexOf(channelItem=>channelItem.name === args.channel);
-      return channels.splice(targetChannelIndex, 1).length > 0;
-
-    }
-
-  },
 };
 
 export default resolvers;
