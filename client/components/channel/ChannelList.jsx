c726d79266a3274abd4b176302263dd4117df974
Rachomir <bciach@thebrain.pro>
2017-03-07 10:00:40 +0000

    Add little fix for styles

--- client/components/channel/ChannelList.jsx
+++ client/components/channel/ChannelList.jsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
-const channelList = [{name: '#general'}, {name: '#theBrain SoftwareHouse'}, {name: '#meteor in flipflops'}];
+const channelList = [{name: '# GENERAL'}, {name: '# THEBRAIN SOFTWARE HOUSE'}, {name: '# MIFF'}];
 
 export class ChannelList extends React.Component {
 
     render() {
         return  <ul className="ChannelList">
-                {channelList.map((channel) => <li key={channel.name}>{channel.name}</li>)}
+            {channelList.map((channel) => <li key={channel.name}>{channel.name}</li>)}
                 </ul>
     }
 }
