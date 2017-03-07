913f09e6ab7545692a7ddbbad9106c09843e0ada
Jan Mozgawa <jmozgawa@gmail.com>
2017-03-07 11:30:37 +0100

    schema

--- server/schema.js
+++ server/schema.js
@@ -1,12 +1,29 @@
 export default `
+
+  type Message {
+    id: ID!
+    handle: String!
+    content: String!
+    timestamp: Int!
+  }
+
+  type Channel {
+    id: ID!
+    name: String!
+    messages: [Message]!
+  }
+
   type Query {
-    [...]
+    channels: [Channel]
+    channel(name: String!): Channel
   }
-
+  
   type Mutation {
-    [...]
+    post(channel: String!, message: String!): Message!
+    createChannel(name: String!): Channel!
+    removeChannel(name: String!): Boolean
   }
-
+  
   schema {
     query: Query
     mutation: Mutation
